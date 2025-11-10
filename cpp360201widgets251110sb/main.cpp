6645dc65ef181fd86722918a166eca474b3c1610
CEP.EBC. <CEPEBC@gmail.com>
2025-11-10 22:02:38 +0300

    Module 36, Lesson 2, Exercise 1: Spinnbox && Slider

--- cpp360201widgets251110sb/main.cpp
+++ cpp360201widgets251110sb/main.cpp
@@ -1,23 +1,28 @@
 #include <QApplication>
-#include <QTimer>
+#include <QSlider>
+#include <QSpinBox>
 #include <iostream>
-
-class Printer : public QObject {
-    Q_OBJECT
-  
-  public slots:
-    void printFinished() {
-        std::cout << "Finished" << std::endl;
-    }
-};
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QTimer myTimer;
-    Printer myPrinter;
-    QObject::connect(&myTimer, &QTimer::timeout, &myPrinter, &Printer::printFinished);
-    myTimer.start(2000);
+    auto* slider = new QSlider(Qt::Horizontal);
+    auto* spinbox = new QSpinBox();
+    slider->setMaximum(100);
+    slider->setMinimum(0);
+    spinbox->setMinimum(0);
+    spinbox->setMaximum(100);
+
+    QObject::connect(slider, &QSlider::valueChanged, spinbox, &QSpinBox::setValue);
+    QObject::connect(spinbox, QOverload<int>::of(&QSpinBox::valueChanged), slider, &QSlider::setValue);
+
+    slider->resize(500, 50);
+    spinbox->resize(200, 50);
+    spinbox->move(2800, 500);
+    slider->move(2600, 500);
+
+    slider->show();
+    spinbox->show();
 
     app.exec();
 }
